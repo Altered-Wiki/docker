ed455a2237869c7b4a7538279f5f93cf01d5a4d4
xenorio <dev@xenorio.xyz>
2026-05-12 15:18:59 +0200

    more search stuff

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -162,10 +162,10 @@ $wgExtraNamespaces[NS_EXPERIENCE_TALK] = 'Experience_talk';
 $smwgNamespacesWithSemanticLinks[NS_SUBSTANCE] = true;
 $smwgNamespacesWithSemanticLinks[NS_RESEARCH]  = true;
 
-# Include custom namespaces in the default search profile
-$wgNamespacesToBeSearchedDefault[NS_SUBSTANCE]  = true;
-$wgNamespacesToBeSearchedDefault[NS_RESEARCH]   = true;
-$wgNamespacesToBeSearchedDefault[NS_EXPERIENCE] = true;
+# Treat custom namespaces as content namespaces (included in default search profile)
+$wgContentNamespaces[] = NS_SUBSTANCE;
+$wgContentNamespaces[] = NS_RESEARCH;
+$wgContentNamespaces[] = NS_EXPERIENCE;
 
 # --- Rights & permissions ---------------------------------------------------
 
